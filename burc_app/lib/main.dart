25ded14b4b1b8dfa157a348a36cbda11ebb05774
Emirorkcu <54988806+Emir-Orkcu@users.noreply.github.com>
2021-11-09 23:26:02 +0300

    .

--- burc_app/lib/main.dart
+++ burc_app/lib/main.dart
@@ -1,27 +1,38 @@
+import 'package:burc_app/burc_detay.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'burc_listesi.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp(),theme: ThemeData(brightness: Brightness.dark),debugShowCheckedModeBanner:false ,));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => BurcListesi()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.brown,brightness: Brightness.dark),
-
-      home: BurcListesi(),
-
-
+    return Scaffold(
+      body:Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/splashscreen4.jpg"),
+                fit: BoxFit.cover)),
+      ),
     );
-   
-
-
   }
 }
